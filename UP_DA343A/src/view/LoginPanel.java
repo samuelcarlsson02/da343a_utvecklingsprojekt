d7c04141cbe1391333d9b51e5105ba311322755e
gihedenalexander <giheden.alexander@gmail.com>
2023-05-18 14:30:59 +0200

    added serverlogger window

--- UP_DA343A/src/view/LoginPanel.java
+++ UP_DA343A/src/view/LoginPanel.java
@@ -1,8 +1,9 @@
+package view;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package up_gui;
 
 /**
  *
