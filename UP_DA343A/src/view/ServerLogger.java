016dde43d6e3364d5b22dbfa79ce189af50f836c
gihedenalexander <giheden.alexander@gmail.com>
2023-05-18 15:35:50 +0200

    deleted clientchat class

--- UP_DA343A/src/view/ServerLogger.java
+++ UP_DA343A/src/view/ServerLogger.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package view;
 
 /**
@@ -154,3 +150,4 @@ public class ServerLogger extends javax.swing.JFrame {
     private javax.swing.JComboBox<String> secondTimeComboBox;
     // End of variables declaration
 }
+
